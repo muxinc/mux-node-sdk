2eb7f5049cc159828970fa9f4ba38d453caf3105
Jacqui Manzi <jacquimanzi@gmail.com>
2018-04-24 11:46:14 -0700

    Fixing axios config

---
+++ test/assets_test.js
@@ -0,0 +1,10 @@
+// const assert = require('assert');
+// const assets = require('../lib/resources/Assets');
+//
+// describe('Assets', () => {
+//     const asset = new assets('testKey', 'testSecret');
+//     describe('create.assets', () => {
+//
+//
+//     });
+// });
