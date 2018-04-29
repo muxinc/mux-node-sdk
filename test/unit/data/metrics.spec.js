348d6c313d5376cbdcfa43df83a263f5b738f231
Jacqui Manzi <jacquimanzi@gmail.com>
2018-04-29 13:46:46 -0700

    Metrics unit tests

--- test/unit/data/metrics.spec.js
+++ test/unit/data/metrics.spec.js
@@ -1,10 +1,14 @@
 const { expect } = require('chai');
 const moxios = require('moxios');
 const sinon = require('sinon');
-const api = require('../../../src/utils/api');
+const Metrics = require('../../../src/data/resources/metrics');
 
-/** @test {Data.metrics} */
-describe.only('Unit::Data.metrics', () => {
+/** @test {Metrics} */
+describe.only('Unit::Metrics', () => {
+  const testApiKey = 'testKey';
+  const testSecret = 'testSecret';
+  const metricsInstance = new Metrics(testApiKey, testSecret);
+
   beforeEach(() => {
     moxios.install();
   });
@@ -12,21 +16,123 @@ describe.only('Unit::Data.metrics', () => {
   afterEach(() => {
     moxios.uninstall();
   });
-  /** @test {Data.metrics} */
-  describe('api', () => {
-    /** @test {Data.metrics.breakdown} */
-    it('makes a get request', (done) => {
-      moxios.stubRequest('https://api.mux.com/data/v1/metrics', {
+
+  /** @test {Metrics} */
+  describe('Metrics', () => {
+    /** @test {Metrics} */
+    it('throws an error if an api key is not given', () => {
+      expect(() => new Metrics()).to.throw('API Access Token must be provided.');
+    });
+
+    /** @test {Metrics} */
+    it('throws an error if a secret key is not given', () => {
+      expect(() => new Metrics(testApiKey)).to.throw('API secret key must be provided');
+    });
+
+    /** @test {Metrics} */
+    it('creates a new Metrics instance', () => {
+      const TestMetrics = new Metrics(testApiKey, testSecret);
+      expect(() => new Metrics(testApiKey, testSecret)).to.not.throw();
+      expect(TestMetrics.requestOptions.auth.username).to.equal(testApiKey);
+      expect(TestMetrics.requestOptions.auth.password).to.equal(testSecret);
+    });
+  });
+
+  /** @test {Metrics.breakdown} */
+  describe('Metrics.breakdown', () => {
+    /** @test {Metrics.breakdown} */
+    it('makes a get request to the Mux data breakdown route', (done) => {
+      moxios.stubRequest('https://api.mux.com/data/v1/metrics/testMetric/breakdown', {
         status: 200,
         responseText: 'breakdown',
       });
 
       const onFulfilled = sinon.spy();
-      api.get('/data/v1/metrics', {}, {})
+      metricsInstance.breakdown('testMetric', {})
         .then(onFulfilled);
 
       return moxios.wait(() => {
         expect(onFulfilled.getCall(0).args[0].data).to.equal('breakdown');
+        done();
+      });
+    });
+  });
+
+  /** @test {Metrics.comparison} */
+  describe('Metrics.comparison', () => {
+    /** @test {Metrics.comparison} */
+    it('makes a get request to the Mux data comparision route', (done) => {
+      moxios.stubRequest('https://api.mux.com/data/v1/metrics/comparison', {
+        status: 200,
+        responseText: 'comparison',
+      });
+
+      const onFulfilled = sinon.spy();
+      metricsInstance.comparison({})
+        .then(onFulfilled);
+
+      return moxios.wait(() => {
+        expect(onFulfilled.getCall(0).args[0].data).to.equal('comparison');
+        done();
+      });
+    });
+  });
+
+  /** @test {Metrics.insights} */
+  describe('Metrics.insights', () => {
+    /** @test {Metrics.insights} */
+    it('makes a get request to the Mux data insights route', (done) => {
+      moxios.stubRequest('https://api.mux.com/data/v1/metrics/testMetric/insights', {
+        status: 200,
+        responseText: 'insights',
+      });
+
+      const onFulfilled = sinon.spy();
+      metricsInstance.insights('testMetric')
+        .then(onFulfilled);
+
+      return moxios.wait(() => {
+        expect(onFulfilled.getCall(0).args[0].data).to.equal('insights');
+        done();
+      });
+    });
+  });
+
+  /** @test {Metrics.overall} */
+  describe('Metrics.overall', () => {
+    /** @test {Metrics.overall} */
+    it('makes a get request to the Mux data overall route', (done) => {
+      moxios.stubRequest('https://api.mux.com/data/v1/metrics/testMetric/overall', {
+        status: 200,
+        responseText: 'overall',
+      });
+
+      const onFulfilled = sinon.spy();
+      metricsInstance.overall('testMetric', {})
+        .then(onFulfilled);
+
+      return moxios.wait(() => {
+        expect(onFulfilled.getCall(0).args[0].data).to.equal('overall');
+        done();
+      });
+    });
+  });
+
+  /** @test {Metrics.timeseries} */
+  describe('Metrics.timeseries', () => {
+    /** @test {Metrics.timeseries} */
+    it('makes a get request to the Mux data timeseries route', (done) => {
+      moxios.stubRequest('https://api.mux.com/data/v1/metrics/testMetric/timeseries', {
+        status: 200,
+        responseText: 'timeseries',
+      });
+
+      const onFulfilled = sinon.spy();
+      metricsInstance.timeseries('testMetric', {})
+        .then(onFulfilled);
+
+      return moxios.wait(() => {
+        expect(onFulfilled.getCall(0).args[0].data).to.equal('timeseries');
         done();
       });
     });
