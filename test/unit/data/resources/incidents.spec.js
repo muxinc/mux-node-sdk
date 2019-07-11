605647f0992ef2847a2569cdbb8f1fd7bb265900
Jacqui Manzi <jacquimanzi@gmail.com>
2019-07-11 13:44:23 -0700

    Adding access to incidents. Updating unit tests.

---
+++ test/unit/data/resources/incidents.spec.js
@@ -0,0 +1,120 @@
+const { expect } = require('chai');
+const moxios = require('moxios');
+const sinon = require('sinon');
+const Incidents = require('../../../../src/data/resources/incidents');
+
+/** @test {Incidents} */
+describe('Unit::Incidents', () => {
+  const testApiKey = 'testKey';
+  const testSecret = 'testSecret';
+  const incidentsInstance = new Incidents(testApiKey, testSecret);
+
+  beforeEach(() => {
+    moxios.install(incidentsInstance.http);
+  });
+
+  afterEach(() => {
+    moxios.uninstall(incidentsInstance.http);
+  });
+
+  /** @test {Incidents} */
+  describe('Incidents', () => {
+    /** @test {Incidents} */
+    it('throws an error if an api key is not given', () => {
+      expect(() => new Incidents()).to.throw('API Access Token must be provided.');
+    });
+
+    /** @test {Incidents} */
+    it('throws an error if a secret key is not given', () => {
+      expect(() => new Incidents(testApiKey)).to.throw(
+        'API secret key must be provided'
+      );
+    });
+
+    /** @test {Incidents} */
+    it('creates a new Incidents instance', () => {
+      const TestErrors = new Incidents(testApiKey, testSecret);
+      expect(() => new Incidents(testApiKey, testSecret)).to.not.throw();
+      expect(TestErrors.tokenId).to.equal(testApiKey);
+      expect(TestErrors.tokenSecret).to.equal(testSecret);
+    });
+  });
+
+  /** @test {Incidents.list} */
+  describe('Incidents.list', () => {
+    /** @test {Incidents.list} */
+    it('throws an error if the status query parameter is not provided', () => {
+      expect(() => incidentsInstance.list()).to.throw(
+        'The status query parameter is required for listing incidents.'
+      );
+      expect(() => incidentsInstance.list({})).to.throw(
+        'The status query parameter is required for listing incidents.'
+      );
+    });
+
+    it('makes a get request to the Mux data incidents route', done => {
+      moxios.stubRequest('https://api.mux.com/data/v1/incidents?status=open', {
+        status: 200,
+        responseText: '{"data": {"incidents": true}}',
+      });
+
+      const onFulfilled = sinon.spy();
+      incidentsInstance.list({status: 'open'}).then(onFulfilled);
+
+      return moxios.wait(() => {
+        expect(onFulfilled.getCall(0).args[0].incidents).to.be.true;
+        done();
+      });
+    });
+  });
+
+  /** @test {Incidents.get} */
+  describe('Incidents.get', () => {
+    /** @test {Incidents.get} */
+    it('throws an error if an incident Id is not provided', () => {
+      expect(() => incidentsInstance.get()).to.throw(
+        'An incident Id is required for incident details.'
+      );
+    });
+
+    it('makes a get request to the Mux data incidents route', done => {
+      moxios.stubRequest('https://api.mux.com/data/v1/incidents/abc123', {
+        status: 200,
+        responseText: '{"data": {"incidents": true}}',
+      });
+
+      const onFulfilled = sinon.spy();
+      incidentsInstance.get('abc123').then(onFulfilled);
+
+      return moxios.wait(() => {
+        expect(onFulfilled.getCall(0).args[0].incidents).to.be.true;
+        done();
+      });
+    });
+  });
+
+  /** @test {Incidents.related} */
+  describe('Incidents.related', () => {
+    /** @test {Incidents.related} */
+    it('throws an error if an incident Id is not provided', () => {
+      expect(() => incidentsInstance.related()).to.throw(
+        'An incident Id is required for related incidents.'
+      );
+    });
+
+    it('makes a get request to the Mux data incidents route', done => {
+      moxios.stubRequest('https://api.mux.com/data/v1/incidents/abc123/related', {
+        status: 200,
+        responseText: '{"data": {"incidents": true}}',
+      });
+
+      const onFulfilled = sinon.spy();
+      incidentsInstance.related('abc123').then(onFulfilled);
+
+      return moxios.wait(() => {
+        expect(onFulfilled.getCall(0).args[0].incidents).to.be.true;
+        done();
+      });
+    });
+  });
+});
