308f3a94a66a38e7302caa227eafb6a37d0a3340
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-05-26 06:34:03 -0700

    got a bunch of interfaces defined

--- types/mux.d.ts
+++ types/mux.d.ts
@@ -26,6 +26,35 @@ declare interface PlaybackId {
   policy: 'public' | 'signed';
 }
 
+declare interface BaseTrack {
+  id: string;
+  passthrough: string;
+  status: TrackStatus;
+}
+
+declare interface AudioTrack extends BaseTrack {
+    type: 'audio';
+    duration: number;
+    max_channels: number;
+    max_channel_layout: TrackMaxChannelLayout;
+}
+
+declare interface TextTrack extends BaseTrack {
+    type: 'text';
+    text_type: 'subtitles';
+    language_code: string;
+    closed_captions: boolean;
+    name: string;
+}
+
+declare interface VideoTrack extends BaseTrack {
+    type: 'video';
+    duration: number;
+    max_width: number;
+    max_height: number;
+    max_frame_rate: number;
+}
+
 type TrackMaxChannelLayout = 'mono' | 'stereo' | '5.1' | '7.1';
 type TrackStatus = 'preparing' | 'ready' | 'errored';
 type TrackType = 'video' | 'audio' | 'text';
@@ -45,11 +74,6 @@ declare interface File {
     filesize: number;
 }
 
-declare interface BaseTrack {
-  id: string;
-  passthrough: string;
-  status: TrackStatus;
-}
 
 declare interface TextTrack {
   type: 'text';
@@ -89,20 +113,182 @@ declare interface Asset {
     errors: AssetError;
 }
 
+declare type PlaybackIdPolicy = 'public' | 'signed';
+
+declare interface InputOverlaySetting {
+    vertical_align?: 'top' | 'middle' | 'bottom';
+    vertical_margin?: string;
+    horizontal_align?: 'left' | 'center' | 'right';
+    horizontal_margin?: string;
+    width?: string;
+    height?: string;
+    opacity?: string;
+}
+
+declare interface Input {
+    url: string;
+    overlay_settings?: InputOverlaySetting;
+    type?: 'video' | 'audio' | 'text';
+    text_type?: string;
+    language_code?: string;
+    name?: string;
+    closed_captions?: boolean;
+    passthrough?: string;
+}
+
+declare interface CreateAssetParams {
+    input: string | Input[];
+    playback_policy?: PlaybackIdPolicy | (PlaybackIdPolicy)[];
+    passthrough?: string;
+    mp4_support?: AssetMp4Support;
+    normalize_audio?: boolean;
+}
+
+declare interface SimulcastTargetParams {
+  url: string;
+  stream_key?: string;
+  passthrough?: string;
+}
+
+declare interface CreateLiveStreamParams {
+    reconnect_window?: number;
+    playback_policy?: PlaybackIdPolicy | (PlaybackIdPolicy)[];
+    new_asset_settings?: any;
+    passthrough?: string;
+    reduced_latency?: boolean;
+    simulcast_targets?: SimulcastTargetParams[]
+    test?: boolean;
+}
+
+declare interface ListParams {
+    limit: number;
+    page: number;
+}
+
+declare interface CreatePlaybackIdParams {
+    policy: PlaybackIdPolicy;
+}
+
+declare class Assets extends Base {
+    create (params: CreateAssetParams): Promise<any>;
+    del (assetId: string): Promise<any>;
+    get (assetId: string): Promise<any>;
+    inputInfo (assetId: string): Promise<any>;
+    list (params: ListParams): Promise<any>;
+}
+
+declare class LiveStreams extends Base {
+    create(params: CreateLiveStreamParams): Promise<any>
+    del(liveStreamId: string): Promise<any>
+    get(liveStreamId: string): Promise<any>
+    list(params: ListParams): Promise<any>;
+    signalComplete(liveStreamId: string): Promise<any>;
+    resetStreamKey(liveStreamId: string): Promise<any>;
+    createPlaybackId(liveStreamId: string, params: CreatePlaybackIdParams): Promise<any>;
+    deletePlaybackId(liveStreamId: string, playbackId: string): Promise<any>;
+    createSimulcastTarget(liveStreamId: string, params: SimulcastTargetParams): Promise<any>
+    getSimulcastTarget(liveStreamId: string, simulcastTargetId: string): Promise<any>;
+    deleteSimulcastTarget(liveStreamId: string, simulcastTargetId: string): Promise<any>;
+}
+
+declare interface CreateUploadParams {
+    timeout?: string;
+    cors_origin?: string;
+    new_asset_settings?: any;
+    test?: boolean;
+}
+
+declare class Uploads extends Base {
+    create(params: CreateUploadParams): Promise<any>;
+    cancel(uploadId: string): Promise<any>;
+    get(uploadId: string): Promise<any>;
+}
+
+declare interface JWTOptions {
+    keyId: string;
+    keySecret: string;
+    type: string;
+    params?: any;
+}
+
+declare class JWT {
+    static sign(playbackId: string, options: JWTOptions): string;
+}
+
+declare class Webhooks {
+    static verifyHeader(payload: string | Buffer, headers: string, secret: string, tolerance?: number): boolean;
+}
+
+declare interface ErrorsParams {
+    filters?: any[];
+    array?: any[];
+}
+
+declare class Errors extends Base {
+    list(params?: ErrorsParams): Promise<any>;
+}
+
+declare class Exports extends Base {
+    list: Promise<any>;
+}
+
+declare class Filters extends Base {
+    list: Promise<any>;
+    get(filterId: string, queryParams?: any): Promise<any>;
+}
+
+declare class Incidents extends Base {
+    list(queryParams?: any): Promise<any>;
+    get(incidentId: string): Promise<any>;
+    related(incidentId: string, queryParams?: any): Promise<any>;
+}
+
+declare class Metrics extends Base {
+    breakdown(metricId: string, queryParams: any): Promise<any>;
+    comparison(queryParams: any): Promise<any>;
+    insights(metricId: string, queryParams?: any): Promise<any>;
+    overall(metricId: string, queryParams?: any): Promise<any>;
+    timeseries(metricId: string, queryParams?: any): Promise<any>;
+}
+
+declare class RealTime extends Base {
+    breakdown(metricId: string, queryParams: any): Promise<any>;
+    dimensions(): Promise<any>;
+    histogramTimeseries(metricId: string, queryParams: any): Promise<any>;
+    metrics(): Promise<any>;
+    timeseries(metricId: string, queryParams: any): Promise<any>;
+}
+
+declare class VideoViews extends Base {
+  get(videoViewId: string): Promise<any>;
+  list(queryParams: any): Promise<any>;
+}
+
 declare class Video extends Base {
+    Assets: Assets;
+    LiveStreams: LiveStreams;
+    Uploads: Uploads;
+    Metrics: Metrics;
+}
+
+declare class Data extends Base {
+    Errors: Errors;
+    Exports: Exports;
+    Filters: Filters;
+    Incidents: Incidents;
+    Metrics: Metrics;
+    RealTime: RealTime;
+    VideoViews: VideoViews;
 }
 
 declare class Mux extends Base {
     constructor(accessTokenOrConfig?: string | RequestOptions, secret?: string, config?: RequestOptions);
     Video: Video;
-  /*
     Data: Data;
     static JWT: typeof JWT;
     static Webhooks: typeof Webhooks;
-  */
 }
 
-declare namespace Mux {
-}
+declare namespace Mux {}
 
 export = Mux;
