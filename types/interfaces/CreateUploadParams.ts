969c925a614bb08296e82864feef28ae9f812bae
Evgenij Beloded <belodedevgenij@gmail.com>
2020-07-06 14:51:11 +0700

    Allow new_asset_settings without 'input'

--- types/interfaces/CreateUploadParams.ts
+++ types/interfaces/CreateUploadParams.ts
@@ -3,6 +3,6 @@ import { CreateAssetParams } from './CreateAssetParams';
 export interface CreateUploadParams {
   timeout?: string;
   cors_origin?: string;
-  new_asset_settings?: CreateAssetParams;
+  new_asset_settings?: Partial<CreateAssetParams>;
   test?: boolean;
 }
