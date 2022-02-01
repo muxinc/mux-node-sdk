676440d59da59f051c5c23e6dc4df33bc897b5e3
Dave Kiss <dave@davekiss.com>
2022-02-01 13:16:11 -0500

    Don't mix CJS and import declarations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Mux } from './mux';
+const Library = require('./mux');
 
 // because we have old clients who are using this, we will still need
 // to use old-style exports at the module edge.
-module.exports = Mux;
+module.exports = Library.Mux;
