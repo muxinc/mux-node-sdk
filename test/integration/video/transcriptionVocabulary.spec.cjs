92d98ffa439a28094f09ee36e08d45e596ea2e97
Dave Kiss <dave@davekiss.com>
2022-07-11 11:34:33 -0400

    Create spec

--- test/integration/video/transcriptionVocabulary.spec.cjs
+++ test/integration/video/transcriptionVocabulary.spec.cjs
@@ -19,4 +19,29 @@ describe('Integration::TranscriptionVocabularies', () => {
       nockDone();
     });
   });
+
+  /** @test {TranscriptionVocabularies.create} */
+  describe('TranscriptionVocabularies.create', () => {
+    /** @test {TranscriptionVocabularies.create} */
+    it('creates a transcription vocabulary with defaults', async () => {
+      const name = 'Mux Test Vocabulary';
+      const phrases = ['test', 'all', 'the', 'things'];
+
+      const { nockDone } = await nockBack(
+        'TranscriptionVocabularies/createWithParams.json'
+      );
+
+      const vocabulary = await Video.TranscriptionVocabularies.create({
+        name,
+        phrases,
+      });
+
+      expect(vocabulary.id).to.exist;
+      expect(vocabulary.name).to.equal(name);
+      expect(vocabulary.phrases).to.be.an('array');
+      expect(vocabulary.phrases).to.have.lengthOf(4);
+      await Video.TranscriptionVocabularies.delete(vocabulary.id);
+      nockDone();
+    });
+  });
 });
