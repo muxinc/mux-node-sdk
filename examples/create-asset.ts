e566bee299665b7522eaf6ba1765099f0ed5b2cb
Alex Rattray <rattray.alex@gmail.com>
2022-10-26 22:37:06 -0400

    Use TS for example

---
+++ examples/create-asset.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env yarn tsn -T
+import Mux from 'mux';
+
+const mux = new Mux({
+  // These are dev env values
+  tokenId: 'b5a8e345-c160-4481-8d83-cf32d0ed1683',
+  tokenSecret: 'DVCYh4ijEQDob+5ZW6BOweLvyDDpCi+r4T/6dcf+K8YylZduiOAK2eS6vdTfauFXg6rldRKSofe',
+});
+
+async function main() {
+  const asset = await mux.video.assets.create({
+    input: [{ url: 'https://storage.googleapis.com/muxdemofiles/mux-video-intro.mp4' }],
+    playback_policy: ['public'],
+  });
+  console.log(asset);
+
+  const assets = [];
+  for await (const asset of mux.video.assets.list()) {
+    console.log(asset.id);
+    assets.push(asset);
+  }
+  console.log(assets.length);
+}
+
+main().catch(console.error);
